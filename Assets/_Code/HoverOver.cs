98fe1f7c265126e892eea769e6436d8d8436a3c3
JelleWho <1545223+jellewie@users.noreply.github.com>
2018-08-08 22:38:11 +0200

    Fixed the hovertext a bit

--- Assets/_Code/HoverOver.cs
+++ Assets/_Code/HoverOver.cs
@@ -6,21 +6,21 @@ using UnityEngine.UI;
 public class HoverOver : MonoBehaviour
 {
     public Text TextBox;
-    public bool RightOfMouse;
+    public GameObject Image;
+    int HorzontalOffzet;
     bool Enabled;
+
+    void Start()
+    {
+        //TextBox = this.gameObject.transform.GetChild(0);
+    }
 
     void Update()
     {
-        Debug.Log("update");
-    }
-
-    void LateUpdate()
-    {
-        Debug.Log("Lateupdate");
         if (Enabled)
         {
-            TextBox.transform.localPosition = new Vector2(
-                 Input.mousePosition.x - Screen.width / 2,
+            Image.transform.localPosition = new Vector2(
+                 Input.mousePosition.x - Screen.width / 2 + HorzontalOffzet,
                  Input.mousePosition.y - Screen.height / 2 - 10
                 );
         }
@@ -28,30 +28,42 @@ public class HoverOver : MonoBehaviour
 
     public void _ShowHoverText(string text)
     {
-        Debug.Log("======================================='" + text + "'");
+        RectTransform m_RectTransform = GetComponent<RectTransform>();
+
         TextBox.text = text;
         if (text == "")
         {
             Enabled = false;
+            Image.SetActive(false);
         }
         else
         {
+            Image.SetActive(true);
             Enabled = true;
             if (text.Substring(0, 1) == "R")
             {
-                Debug.Log("Right");
-                TextBox.alignment = TextAnchor.MiddleLeft; ;
+                HorzontalOffzet = 150;
+                TextBox.alignment = TextAnchor.MiddleLeft;
                 TextBox.text = text.Substring(1, text.Length -1);
+
+
+                m_RectTransform.anchorMin = new Vector2(0, 0.5f);
+                m_RectTransform.anchorMax = new Vector2(0, 0.5f);
             }
             else if (text.Substring(0, 1) == "L")
             {
-                Debug.Log("Left");
+                HorzontalOffzet = -100;
                 TextBox.alignment = TextAnchor.MiddleRight;
                 TextBox.text = text.Substring(1, text.Length - 1);
+
+
+                
+                m_RectTransform.anchorMin = new Vector2(1, 0.5f);
+                m_RectTransform.anchorMax = new Vector2(1, 0.5f);
             }
             else
             {
-                TextBox.text = text;//
+                TextBox.text = text;
                 Debug.LogError("No direction of HoverText given, using the last know direction");
             }
         }
