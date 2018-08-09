9660d3aed82e0891d70d0d0a4ba9e088b03b58f4
jellewie <J.Wietsma@synspec.nl>
2018-08-09 10:01:56 +0200

    HoverOver is now working with a start bug
    on the first frame the image hasn't been resized yet, and thus the position is still off. this hasn't been fixed. but the HoverOver now works for the Settings icon and BuildMenu icons

--- Assets/_Code/HoverOver.cs
+++ Assets/_Code/HoverOver.cs
@@ -5,67 +5,70 @@ using UnityEngine.UI;
 
 public class HoverOver : MonoBehaviour
 {
-    public Text TextBox;
-    public GameObject Image;
-    int HorzontalOffzet;
+    public Text TextBox;                                                                        //The child textbox
+    public GameObject img;                                                                      //The parrent image
+    public RectTransform Image_RectTransform;                                                   //The parrent image
+    private sbyte HorzontalOffzet;                                                              //Offset direction
     bool Enabled;
 
     void Start()
     {
-        //TextBox = this.gameObject.transform.GetChild(0);
+        img = this.gameObject;                                                                  //Set the gameObject link (Needed to enable / disable)
+        img.SetActive(false);                                                                   //The text/image should be hidden by default
+        Image_RectTransform = GetComponent<RectTransform>();                                    //Set the RectTransform link (Needed for size measurement        
+        TextBox = this.GetComponentInChildren<Text>();                                          //Set the Textbox link (This text will be changed)
     }
-
-    void Update()
+    void Update()                   //Update every frame (only if active)
     {
-        if (Enabled)
-        {
-            Image.transform.localPosition = new Vector2(
-                 Input.mousePosition.x - Screen.width / 2 + HorzontalOffzet,
-                 Input.mousePosition.y - Screen.height / 2 - 10
-                );
-        }
+        img.SetActive(true);
+        MoveToCursor();                                                                     //Move it to the cursor
     }
+    public void MoveToCursor()
+    {
+        /*
+        The "Image_RectTransform.rect.width" isn't updated yet the first time we call this with "_ShowHoverText"
+        Thats wby it's blinking once. Not sure how to fix this
 
+
+         */
+        img.transform.localPosition = new Vector2(
+            Input.mousePosition.x - Screen.width / 2 + HorzontalOffzet * (Image_RectTransform.rect.width / 2 + 10),
+            Input.mousePosition.y - Screen.height / 2 - 10
+            );
+    }
     public void _ShowHoverText(string text)
     {
-        RectTransform m_RectTransform = GetComponent<RectTransform>();
-
-        TextBox.text = text;
+        Debug.Log(text);
         if (text == "")
         {
-            Enabled = false;
-            Image.SetActive(false);
+            img.SetActive(false);
         }
         else
         {
-            Image.SetActive(true);
-            Enabled = true;
-            if (text.Substring(0, 1) == "R")
+            if (text.Substring(0, 1) == "R")                                                    //If text need to be on the Right side of the cursor
             {
-                HorzontalOffzet = 150;
-                TextBox.alignment = TextAnchor.MiddleLeft;
-                TextBox.text = text.Substring(1, text.Length -1);
-
-
-                m_RectTransform.anchorMin = new Vector2(0, 0.5f);
-                m_RectTransform.anchorMax = new Vector2(0, 0.5f);
+                TextBox.alignment = TextAnchor.MiddleLeft;                                      //Set the text aligment on the Left (that is closest to the mouse)
+                TextBox.text = text.Substring(1, text.Length -1);                               //Set the text
+                HorzontalOffzet = 1;                                                            //Set the offset direction
+                //Image_RectTransform.anchorMin = new Vector2(0, 0.5f);
+                //Image_RectTransform.anchorMax = new Vector2(0, 0.5f);
             }
-            else if (text.Substring(0, 1) == "L")
+            else if (text.Substring(0, 1) == "L")                                               //If text need to be on the Left side of the cursor
             {
-                HorzontalOffzet = -100;
-                TextBox.alignment = TextAnchor.MiddleRight;
-                TextBox.text = text.Substring(1, text.Length - 1);
+                TextBox.alignment = TextAnchor.MiddleRight;                                     //Set the text aligment on the Right (that is closest to the mouse)
+                TextBox.text = text.Substring(1, text.Length - 1);                              //Set the text
 
+                HorzontalOffzet = -1;                                                           //Set the offset direction
 
-                
-                m_RectTransform.anchorMin = new Vector2(1, 0.5f);
-                m_RectTransform.anchorMax = new Vector2(1, 0.5f);
+                Image_RectTransform.anchorMin = new Vector2(1, 0.5f);
+                Image_RectTransform.anchorMax = new Vector2(1, 0.5f);
             }
             else
             {
                 TextBox.text = text;
                 Debug.LogError("No direction of HoverText given, using the last know direction");
             }
+            img.SetActive(true);
         }
     }
 }
