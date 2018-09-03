2cd8279120f94debf6d882a7d5fa1a07fa39227a
JelleWho <1545223+jellewie@users.noreply.github.com>
2018-09-03 18:57:41 +0200

    Added health tag for buildings

--- Assets/_Code/Standalone/BuildingOption.cs
+++ Assets/_Code/Standalone/BuildingOption.cs
@@ -3,13 +3,17 @@ using System.Collections;                                                       
 
 public class BuildingOption : MonoBehaviour {
     public string BuildingName;                                                                 //The name of the building (used for the lookup)
-    public byte SelectedOption;                                                                 //Which option is selected (if this option has options, examples Lumberjack(ox), castle(tax), ox(trapsport), etc
-    public bool Used;                                                                           //If this object is used (if not you can get more for removing it)
     public byte FlagAsUsedAfterSeconds;                                                         //Flag the object as used after this amount of seconds (0 to ignore) This will effect the deconstruct return
+    public byte MaxHealth = 100;                                                                //The maximum (and start) health of the building
+
+    public bool Used;                                                                           //If this object is used (if not you can get more for removing it)
     public bool Active;                                                                         //Flag false when building, and when sleeping
+    public byte SelectedOption;                                                                 //Which option is selected (if this option has options, examples Lumberjack(ox), castle(tax), ox(trapsport), etc
+    public byte Health;                                                                         //The current health of th building
 
     public void StartTimer()                                                            //This code will start the 'Used' after x seconds timer
     {
+        Health = MaxHealth;                                                                     //Set the current health level to max
         if (FlagAsUsedAfterSeconds > 0)                                                         //If used has been flagged. This is called upon placement
         {
             StartCoroutine(ExecuteAfterTime(FlagAsUsedAfterSeconds));                           //Start a Coroutine to trigger
