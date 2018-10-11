1bb068087835256c63b58ad95626928d7325f604
JelleWho <1545223+jellewie@users.noreply.github.com>
2018-10-11 22:18:52 +0200

    Updated comentary of minimap a bit (Credits to @jorisvaneijden for the code!)
    I'm just to autistic to let it slip xD

--- Assets/_Code/Standalone/Minimap.cs
+++ Assets/_Code/Standalone/Minimap.cs
@@ -6,19 +6,18 @@ using PublicCode;
 public class Minimap : MonoBehaviour {
     public Camera FollowCam;                                                            //The camera to follow
 
-    private Plane groundPlane = new Plane(Vector3.up, Vector3.zero);  // A mathematical construct to represent the "floor" of the world
+    private Plane groundPlane = new Plane(Vector3.up, Vector3.zero);                    //A mathematical construct to represent the "floor" of the world
 
     void LateUpdate()                                                                   //Called after each frame after Update
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));  // Cast a ray to the center of the camera
-        float distance;
-        if (groundPlane.Raycast(ray, out distance)) // Get where the ray intersects the ground
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));                   //Cast a ray to the center of the camera
+        float distance;                                                                         //The distance from the screen to the ground plane
+        if (groundPlane.Raycast(ray, out distance))                                             //Get where the ray intersects the ground
         {
-            Vector3 newPosition = Vector3.ClampMagnitude(ray.GetPoint(distance), 1.5f * JelleWho.MaxMoveHorizontalOnMap);  // Clamp to a circle around the center of the world
-            FollowCam.transform.position = new Vector3(newPosition.x, FollowCam.transform.position.y, newPosition.z); // Let the minimap follow where the camera is looking
+            Vector3 newPosition = Vector3.ClampMagnitude(ray.GetPoint(distance), 1.5f * JelleWho.MaxMoveHorizontalOnMap); //Clamp to a circle around the center of the world
+            FollowCam.transform.position = new Vector3(newPosition.x, FollowCam.transform.position.y, newPosition.z); //Let the minimap follow where the camera is looking
         }
-
-        FollowCam.transform.rotation = Quaternion.Euler(90f, Camera.main.transform.eulerAngles.y, 0f); //Also follow the angle
+        FollowCam.transform.rotation = Quaternion.Euler(90f, Camera.main.transform.eulerAngles.y, 0f); //Also let the minimap follow the angle of the camera
         if (EnableZoom)                                                                         //If Zoom is enabled
         {
             float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");                       //Get the scrollwheel location
