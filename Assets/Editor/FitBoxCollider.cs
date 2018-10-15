9fd5dd9db31e3c1fa39daa7da7a8312c143c4bbd
JorisVanEijden <joris.vaneijden@gmail.com>
2018-10-15 14:23:59 +0200

    Add tool to tools menu to auto-size box colliders
    Usage:
    - Select gameobject(s)
    - Add a new BoxCollider if needed
    - Go to menu->Tools->BoxCollider->Fit to children
    The BoxCollider will be scaled to precisely fit the object.

---
+++ Assets/Editor/FitBoxCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class FitBoxCollider
+{
+    [MenuItem("Tools/BoxCollider/Fit to Children")]
+    private static void FitToChildren()
+    {
+        foreach (GameObject gameObject in Selection.gameObjects)
+        {
+            BoxCollider collider = gameObject.GetComponent<BoxCollider>();
+            if (collider != null)
+            {
+                bool hasBounds = false;
+                Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+
+                foreach (Renderer childRenderer in gameObject.GetComponentsInChildren<Renderer>())
+                {
+                    if (hasBounds)
+                    {
+                        bounds.Encapsulate(childRenderer.bounds);
+                    }
+                    else
+                    {
+                        bounds = childRenderer.bounds;
+                        hasBounds = true;
+                    }
+                }
+                collider.center = bounds.center - gameObject.transform.position;
+                collider.size = bounds.size;
+            }
+        }
+    }
+}
