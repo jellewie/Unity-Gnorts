bdd43f09ef6db6130d8719d6e75de18b47e54cc7
Andrei <andreiserbanrazvan@gmail.com>
2022-03-05 13:34:03 +0200

    Added state machine suport for future ai & fixed bugged materials on keep building

---
+++ Assets/_Code/FSM.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+
+namespace FiniteStateMachine
+{
+    public class State
+    {
+        protected FSM m_fsm;
+        public State(FSM fsm)
+        {
+            m_fsm = fsm;
+        }
+
+        public virtual void Enter() { }
+        public virtual void Exit() { }
+        public virtual void Update() { }
+        public virtual void FixedUpdate() { }
+    }
+
+    public class FSM
+    {
+        protected Dictionary<int, State> m_states = new Dictionary<int, State>();
+        protected State m_currentState;
+
+        public FSM()
+        {
+        }
+
+        public void Add(int key, State state)
+        {
+            m_states.Add(key, state);
+        }
+
+        public State GetState(int key)
+        {
+            return m_states[key];
+        }
+
+        public void SetCurrentState(State state)
+        {
+            if (m_currentState != null)
+            {
+                m_currentState.Exit();
+            }
+
+            m_currentState = state;
+
+            if (m_currentState != null)
+            {
+                m_currentState.Enter();
+            }
+        }
+
+        public void Update()
+        {
+            if (m_currentState != null)
+            {
+                m_currentState.Update();
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (m_currentState != null)
+            {
+                m_currentState.FixedUpdate();
+            }
+        }
+    }
+}
+    
+
