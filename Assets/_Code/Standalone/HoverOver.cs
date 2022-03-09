a98bfbb9db7d296b5fcd965b271667cb20e3479d
Jelle Wie¿ <1545223+jellewie@users.noreply.github.com>
2022-03-09 15:36:07 +0100

    Hoverover-text fix
    In free Aspect it didn't always follow the mouse correctly, that is now fixed.
    
    Also fixed that the distance from the mouse to the box looks the same on all resolutions

--- Assets/_Code/Standalone/HoverOver.cs
+++ Assets/_Code/Standalone/HoverOver.cs
@@ -11,16 +11,17 @@ IBeginDragHandler,
 IDragHandler, 
 IEndDragHandler
 {
+    Camera uiCamera;                                                                            //This sh*t is unused, but was required by ScreenPointToLocalPointInRectangle
     public GameObject Prefab;                                                                   //The preab that needs to be placed when clicked on     Leave emthy to not place anything
     public string Direction = "L";                                                              //The direction of the text                             Can be; Left Righ Up Down
     public string HoverText;                                                                    //The text to show
-
     private GameObject ParentHoverOver;                                                         //The parrent
     private GameObject OBJHoverOver;                                                            //The child we are going to move and enable/disable (We can't to the parent or we will lose track of the parent)
     private UserInput CodeUserInput;                                                            //The UserInput code
     private Text TextBox;                                                                       //The child textbox
     private bool MouseIsOver;                                                                   //A bool that keeps track if the mouse is over this object
-    private Vector2 HorzontalOffzet;                                                            //Offset direction
+    private Vector2 OffsetTextBox;                                                              //Offset direction
+    private Vector2 OffsetScaler;                                                               //Howmuch to scale the offset from mouse to text with compaired to a 1920*1080 screen
     private void Start()                                                                //Run once on startup
     {
         ParentHoverOver = GameObject.Find("HoverOver");                                         //Get the Code HoverOver
@@ -30,13 +31,6 @@ IEndDragHandler
         TextBox = OBJHoverOver.GetComponentInChildren<Text>();                                  //Set the Textbox reference (This text will be changed)
         if(HoverText == "" && Prefab != null)                                                   //If no HoverText is given, and there is a Prefab
             HoverText = Prefab.name;                                                            //Use the Prefab name 
-    }
-    private void MoveToCursos()                                                         //Move the box to the mouse
-    {
-        OBJHoverOver.transform.localPosition = new Vector2(                                     //Let the box follow the mouse around
-            Input.mousePosition.x - Screen.width / 2 + HorzontalOffzet.x,                       //X position of box
-            Input.mousePosition.y - Screen.height / 2 - 10 + HorzontalOffzet.y                  //Y position of box
-            );
     }
     private RectTransform Image_RectTransform;                                          //The parrent image
     public void OnPointerEnter(PointerEventData evd)                                    //Run each time the mouse enters this object
@@ -51,7 +45,7 @@ IEndDragHandler
         TextBox.fontSize = 25;
         if (Prefab != null)                                                                     //If a Prefab is set
         {
-            BuildingInfo BuildingInfo = BuildingData.GetInfo(Prefab.name);                //Get the building info
+            BuildingInfo BuildingInfo = BuildingData.GetInfo(Prefab.name);                      //Get the building info
             if (BuildingInfo.Name != "N/A")                                                     //If we have not encountered an error
             {
                 if (BuildingInfo.Cost.Wood > 0)                                                 //If this building needs wood
@@ -68,28 +62,29 @@ IEndDragHandler
                 }
             }
         }
+        OffsetScaler = new Vector2(Screen.height / 1080f, Screen.width / 1920f);
+
         if (Direction == "R")                                                                   //If text need to be on the Right side of the cursor
         {
             Image_RectTransform.pivot = new Vector2(0, 0.5f);                                   //Set the pivit point (coords 0,0)
-            HorzontalOffzet = new Vector2(10, 0);                                               //Set the offset (So we are not below the cursor)
+            OffsetTextBox = new Vector2(10, 0) * OffsetScaler;                                  //Set the offset (So we are not below the cursor)
         }
         else if (Direction == "L")                                                              //If text need to be on the Left side of the cursor
         {
             Image_RectTransform.pivot = new Vector2(1, 0.5f);                                   //Set the pivit point (coords 0,0)
-            HorzontalOffzet = new Vector2(-10, 0);                                              //Set the offset (So we are not below the cursor)
+            OffsetTextBox = new Vector2(-10, 0) * OffsetScaler;                                 //Set the offset (So we are not below the cursor)
         }
         else if (Direction == "U")                                                              //If text need to be on the top (Up) side of the cursor
         {
             Image_RectTransform.pivot = new Vector2(0.5f, 0);                                   //Set the pivit point (coords 0,0)
-            HorzontalOffzet = new Vector2(0, 20);                                               //Set the offset (So we are not below the cursor)
+            OffsetTextBox = new Vector2(0, 20) * OffsetScaler;                                  //Set the offset (So we are not below the cursor)
         }
         else                                                                                    //If text need to be on the bottom (Down) side of the cursor
         {
             Image_RectTransform.pivot = new Vector2(0.5f, 1);                                   //Set the pivit point (coords 0,0)
-            HorzontalOffzet = new Vector2(0, -20);                                              //Set the offset (So we are not below the cursor)
+            OffsetTextBox = new Vector2(0, -20) * OffsetScaler;                                 //Set the offset (So we are not below the cursor)
         }
         OBJHoverOver.SetActive(true);
-        MoveToCursos();                                                                         //Move the box to the mouse
     }
     public void OnPointerExit(PointerEventData evd)                                     //Run each time the mouse leaves this object
     {
@@ -104,20 +99,21 @@ IEndDragHandler
     private void Update()                                                               //Run each frame
     {
         if (MouseIsOver)                                                                        //If the mouse is over this object
-            MoveToCursos();                                                                     //Move the HoverOver box to the mouse
+        {               
+            Vector2 LocalPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(OBJHoverOver.transform.parent.GetComponent<RectTransform>(), Input.mousePosition, uiCamera, out LocalPoint);
+            OBJHoverOver.transform.localPosition = LocalPoint + OffsetTextBox;                  //Move the HoverOver box to the mouse
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)                                 //Run before a drag is started
     {
         CodeUserInput.StartDragging();
     }
-
     public void OnDrag(PointerEventData eventData)                                      //Run when dragging and the cursor is moved
     {
-        // Empty implementation of IDragHandler.
-        // Needed for OnBeginDrag and OnEndDrag to function.
+        // Empty implementation of IDragHandler. Needed for OnBeginDrag and OnEndDrag to function.
     }
-
     public void OnEndDrag(PointerEventData eventData)                                   //Run when a drag is ended
     {
         CodeUserInput.StopDragging();
